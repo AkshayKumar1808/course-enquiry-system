28c100ae2853d4ee90b80b4d8ef8279bc848b5dd
AkshayKumar1808 <akshayfagare7162@gmail.com>
2025-04-06 12:52:05 +0530

    add the comman libraries

---
+++ comman-library/libraries/src/main/java/com/vita/libraries/model/ResponseModel.java
@@ -0,0 +1,22 @@
+package com.vita.libraries.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResponseModel<T> {
+	
+	private Object obj;
+	
+	@Builder.Default
+	private int exceptionCode=-1;
+	
+	@Builder.Default
+	private String exceptionDescription="No such exception occured";
+	
+	private String message;
+	
+}
